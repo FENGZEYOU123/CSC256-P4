ef7201b6c5a219e2e34573987d4ea8fffb15334e
Anthony <ajsouza88@gmail.com>
2018-03-13 17:25:40 -0700

    Add files via upload

---
+++ p4codeBase.s
@@ -0,0 +1,80 @@
+.data
+    endl:    .asciiz  "\n"   # used for cout << endl;
+    sumlbl:    .asciiz  "Sum: " # label for sum
+    revlbl:    .asciiz  "Reversed Number: " # label for rev
+    pallbl:    .asciiz  "Is Palindrome: " # label for isPalindrome
+    arr:       .word 1
+               .word 2
+               .word 3
+               .word 4
+               .word 5
+               .word 4
+               .word 3
+               .word 2
+               .word 1
+    sumarr:    .word 1
+               .word 3
+               .word 44
+               .word 66
+               .word 88
+               .word 90
+               .word 9
+               .word 232
+               .word 4325
+               .word 2321
+.text
+
+# sum            --> $s0
+# size           --> $s1
+# rev            --> $s2
+# num            --> $s3
+# isPalindrome   --> $s4
+# address of arr --> $s5
+# i              --> $s6
+# beg            --> $s7
+# end            --> $s8
+# d              --> $t0
+# 10             --> $t1
+# 100            --> $t3
+main:
+
+exit:
+  la   $a0, sumlbl    # puts sumlbl into arg0 (a0 register) for cout
+  addi $v0, $0, 4     # puts 4 in v0 which denotes we are printing a string
+  syscall             # make a syscall to system
+
+  move $a0, $s0       # puts sum into arg0 (a0 register) for cout
+  addi $v0, $0, 1     # puts 1 in v0 to denote we are printing an int
+  syscall             # make a syscall to system
+
+  la   $a0, endl      # puts the address of the string endl into a0
+  addi $v0, $0, 4     # puts 4 into v0 saying we are printing a string
+  syscall
+
+  la   $a0, revlbl    # puts revlbl into arg0 (a0 register) for cout
+  addi $v0, $0, 4     # puts 4 in v0 which denotes we are printing an string
+  syscall             # make a syscall to system
+
+  move $a0, $s1       # puts rev into arg0 (a0 register) for cout
+  addi $v0, $0, 1     # puts 1 in v0 to denote we are printing an int
+  syscall             # make a syscall to system
+
+  la   $a0, endl      # puts the address of the string endl into a0
+  addi $v0, $0, 4     # puts 4 into v0 saying we are printing a string
+  syscall
+
+  la   $a0, pallbl    # puts pallbl into arg0 (a0 register) for cout
+  addi $v0, $0, 4     # puts 4 in v0 which denotes we are printing a string
+  syscall             # make a syscall to system
+
+  move $a0, $s3       # puts isPalindrome into arg0 (a0 register) for cout
+  addi $v0, $0, 1     # puts 1 in v0 to denote we are printing an int
+  syscall             # make a syscall to system
+
+  la   $a0, endl      # puts the address of the string endl into a0
+  addi $v0, $0, 4     # puts 4 into v0 saying we are printing a string
+  syscall
+
+
+  addi $v0,$0, 10
+  syscall
