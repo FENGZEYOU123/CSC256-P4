b748cb596b199ab432bee8b09ea911c4d714c3f9
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2018-03-28 00:53:22 -0700

    Update p4codeBase.s

--- p4codeBase.s
+++ p4codeBase.s
@@ -37,8 +37,57 @@
 # d                 --> $t1
 # 10                --> $t2
 # 100               --> $t3
-main:
 
+main:
+    li $s0, 0       #int sum = 0;
+    li $s5, 1       #int i = 1
+    li $t3, 100
+loop:
+    bge $s5, $t3, loop2        #i <= 100
+    add $s0, $s0, $s5          #sum = sum + i
+    addi $s5, $s5, 1           #i++
+    j loop
+
+    li $s2, 45689   #int num = 45689
+    li $s1, 0       #int rev = 0
+    li $t0, -1      #int d = -1
+    li $t1, 10
+    ble $s2, 0, loop3   #( num > 0)
+loop2:
+
+    rem $t0, $s2, $t1   #d = num % 10
+
+    mul $t4, $s1, $t1   # store in t4 rev*10
+    add $s1, $t4, $t0   #rev = rev*10 + d
+    div $s2, $s2, $t1   #num = num / 10
+    j loop2
+
+    li $s6, 0       #int beg = 0
+    li $s7, 8       #int end = 8
+    li $s3, 1       #int isPalindrome = 1
+    la $s4, arr     #address for array
+
+    bge $s6,$s7, exit #while(beg < end)
+
+loop3:
+
+    lw $s6, 0
+    lw $s7, 8
+    sll $t5, $s6, 4
+    add $t5, $t5, $s4
+    lw $t6, 0($t5)
+
+    sll $t7, $s7, 4
+    add $t7, $t7, $s4
+    lw $t8, 0($t7)
+
+    ble $t6, $t8,if    # if (arr[beg] != arr[end]){
+    li $s3, -1
+    j loop3
+if:
+    addi $s6, $s6, 1
+    addi $s7, $s7, -1
+    
 exit:
   la   $a0, sumlbl    # puts sumlbl into arg0 (a0 register) for cout
   addi $v0, $0, 4     # puts 4 in v0 which denotes we are printing a string
