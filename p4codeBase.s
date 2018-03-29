5cf9731246630143d15fbd2145d3836348d82c8b
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2018-03-29 13:22:28 -0700

    Update p4codeBase.s

--- p4codeBase.s
+++ p4codeBase.s
@@ -45,27 +45,27 @@ main:
     li $t3, 100
 
 loop:
-    bge $s5, $t3, loop2        #i <= 100
-    add $s0, $s0, $s5          #sum = sum + i
-    addi $s5, $s5, 1           #i++
+    bge $t0, $t3, loop2        #i <= 100
+    add $s0, $s0, $t0          #sum = sum + i
+    addi $t0, $t0, 1           #i++
     j loop
 
-    li $s2, 45689   #int num = 45689
-    li $s1, 0       #int rev = 0
-    li $t0, -1      #int d = -1
-    ble $s2, 0, loop3   #( num > 0)
+    li $s3, 45689   #int num = 45689
+    li $s2, 0       #int rev = 0
+    li $t1, -1      #int d = -1
+    ble $s3, 0, loop3   #( num > 0)
 loop2:
 
-    rem $t0, $s2, $t1   #d = num % 10
+    rem $t1, $s3, $t2   #d = num % 10
 
-    mul $t4, $s1, $t1   # store in t4 rev*10
-    add $s1, $t4, $t0   #rev = rev*10 + d
-    div $s2, $s2, $t1   #num = num / 10
+    mul $t4, $s2, $t2   # store in t4 rev*10
+    add $s2, $t4, $t1   #rev = rev*10 + d
+    div $s3, $s3, $t2   #num = num / 10
     j loop2
 
     li $s6, 0       #int beg = 0
     li $s7, 8       #int end = 8
-    li $s3, 1       #int isPalindrome = 1
+    li $s4, 1       #int isPalindrome = 1
     la $s4, arr     #address for array
 
     bge $s6,$s7, exit #while(beg < end)
@@ -74,8 +74,8 @@ loop3:
 
     lw $s6, 0
     lw $s7, 8
-    sll $t5, $s6, 4
-    add $t5, $t5, $s4
+    sll $t0, $s6, 4
+    add $t0, $t5, $s4
     lw $t6, 0($t5)
 
     sll $t7, $s7, 4
@@ -83,7 +83,7 @@ loop3:
     lw $t8, 0($t7)
 
     ble $t6, $t8,if    # if (arr[beg] != arr[end]){
-    li $s3, -1
+    li $s4, -1
     j loop3
 if:
     addi $s6, $s6, 1
