681032c750f2228c5c92adecf1ca33429bea0796
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2018-04-01 22:06:46 -0700

    Update p4codeBase.s

--- p4codeBase.s
+++ p4codeBase.s
@@ -39,19 +39,31 @@
 # 100               --> $t3
 
 main:
-   li $s0, 0
-   la $s1, sumarr
-   li $t0, 0
- Loop:  
-       beq $t0, 10, EndLoop  #if i >=0
+   li $s0, 0        # sum=0
+   la $s1, sumarr   # load sumarr to S1
+   li $t0, 0        # i=0
+   li $s2, 0        # rev=0
+   li $s3, 45689    # num=45689
+   li $t1, -1       # d=-1
+   li $t2, 10       # $st=10
+ Loop1:  
+       bge $t0, 10, EndLoop1  #if i >=0 endloop
        sll $t4, $t0, 2       # indexable i
        add $t4, $t4, $s1
-       lw $t4, 0($t4)
+       lw  $t4, 0($t4)
        add $s0, $s0, $t2
        addi $t0, $t0, 1
-j Loop
-       
-EndLoop:
+j Loop1     
+EndLoop1:
+
+Loop2：
+       ble $s3, $t0, EndLoop2 #if num <=0
+       rem $t1, $s3, $t2        # d=num%10
+       mul $s2, $s2, $t2        # rev=rev*10
+       addi $s2, $s2, $t1       # rev=rev+d
+       div $s3, $s3, $t2        # num/10
+j Loop2
+EndLoop2:
    
 exit:
   la   $a0, sumlbl    # puts sumlbl into arg0 (a0 register) for cout
